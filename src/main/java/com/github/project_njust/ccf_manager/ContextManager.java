4c9e9069a4db05129dac59899dde63ee1909010d
BryanSer <bryan_lzh@njust.edu.cn>
2019-11-29 10:39:29 +0800

    init

---
+++ src/main/java/com/github/project_njust/ccf_manager/ContextManager.java
@@ -0,0 +1,23 @@
+package com.github.project_njust.ccf_manager;
+
+import org.apache.log4j.Logger;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class ContextManager implements ServletContextListener {
+    private static ServletContext servletContext;
+
+    public static ServletContext getServletContext() {
+        return servletContext;
+    }
+
+    public void contextInitialized(ServletContextEvent evt) {
+        servletContext = evt.getServletContext();
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+    }
+}
