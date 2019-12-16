812639889d4799b466afd976668fcb4c8f32a84f
linapz <linapz@qq.com>
2019-12-16 20:27:52 +0800

    (1)修改管理员页面菜单条目名称 (2)新增"新增选拔信息" js代码

---
+++ src/main/webapp/js/admin_timeSet.js
@@ -0,0 +1,35 @@
+const app = new Vue({
+    el: "#VipSetUp",
+    data: {
+        signUpTime: '',
+        scoreLine: ''
+    },
+    methods: {
+        resetSignUpTime() {
+            this.signUpTime = ''
+        },
+        resetScoreLine() {
+            this.scoreLine = ''
+        },
+        addExam() {
+            axios({
+                url: "/Data/addExam",
+                methods: "POST",
+                data: {
+                    token: getCookie("token"),
+                    signUpTime: this.signUpTime,
+                    scoreLine: this.scoreLine
+                }
+            }).then(function (rep) {
+                if (rep.data.status === 'SUCCESS') {
+                    setCookie("token", rep.data.token);
+                    alert("新增选拔信息成功");
+                } else {
+                    alert("新增选拔信息失败" + rep.data.reason);
+                }
+            }, function () {
+                alert("抱歉，服务当前不可用");
+            })
+        }
+    }
+});
