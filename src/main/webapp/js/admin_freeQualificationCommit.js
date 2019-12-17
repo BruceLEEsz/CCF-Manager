fcdff54a1611cb1e59204d29e92fecd83550f999
李尚哲 <bruceleesz@outlook.com>
2019-12-17 17:52:57 +0800

    增加文件上传功能

---
+++ src/main/webapp/js/admin_freeQualificationCommit.js
@@ -0,0 +1,47 @@
+let formData = new window.FormData();
+const app = new Vue({
+    el: "#upload",
+    data: {
+        formData: formData
+    },
+    methods: {
+        upload() {
+            this.formData.append('userfile', document.getElementById('freeQualification').files[0]);
+            this.formData.append("token", getCookie("token"));
+            axios({
+                url: "/File/upload",
+                methods: "POST",
+                data: formData
+            }).then(function (rep) {
+                    if (rep.data.status === 'SUCCESS') {
+                        setCookie("token", rep.data.token);
+                        let UUID = rep.data.UUID;
+                        axios({
+                            url: "/Data/updataFinalList",
+                            method: "POST",
+                            data: {
+                                token: getCookie("token"),
+                                params: {
+                                    UUID: UUID
+                                }
+                            }
+                        }).then(function (rep) {
+                            if (rep.data.status === "SUCCESS") {
+                                setCookie("token", rep.data.token);
+                                alert("免费名单上传成功");
+                            } else {
+                                alert("免费名单上传失败" + rep.data.reason);
+                            }
+                        }, function () {
+                            alert("抱歉，服务器当前不可用");
+                        })
+                    } else {
+                        alert("免费名单上传失败" + rep.data.reason);
+                    }
+                }, function () {
+                    alert("抱歉，服务器当前不可用");
+                }
+            )
+        }
+    }
+});
